c66494163cd351be0f00123e846bac76c426df92
ajvictorio0109 <eijay0109@gmail.com>
2025-08-23 02:28:53 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
+module.exports = {
+  output: 'export',
+  trailingSlash: true
 }
-
-module.exports = nextConfig
